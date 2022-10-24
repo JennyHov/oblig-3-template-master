a932d400d0b0d2a6210d29eafb43cb0fd9a8fceb
JennyHov <s364545@oslomet.no>
2022-10-24 15:14:04 +0200

    Kjørt kode uten unntak

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -149,14 +149,15 @@ public class SBinTre<T> {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
-    /*
-    public static void main(String[] args){
-        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+
+    public static void main(String[] args) {
+        Integer[] a = {4, 7, 2, 9, 5, 10, 8, 1, 3, 6};
         SBinTre<Integer> tre = new SBinTre<>(Comparator.naturalOrder());
-        for (int verdi : a) {tre.leggInn(verdi); }
+        for (int verdi : a) {
+            tre.leggInn(verdi);
+        }
         System.out.println(tre.antall());
     }
-     */
 
 
 } // ObligSBinTre
